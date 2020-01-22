399b04210b39b662c8c343a1eeaf06a66fd3d2f0
luosizhu <2812047004@qq.com>
2020-01-22 23:05:30 +0800

    第一次提交github01

---
+++ github01/src/main/java/com/luo/domain/Account.java
@@ -0,0 +1,14 @@
+package com.luo.domain;
+
+import java.io.Serializable;
+
+/**
+ * @Company：http://www.xxx.com
+ * @Auto：luosizhu
+ * @Date：2020/1/22 22:49
+ * @Description：com.luo.domain
+ */
+public class Account implements Serializable {
+    private Integer number;
+    private String name;
+}
